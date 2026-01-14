38cb093e89a1e5bd82ae2e2fb4ea774ab6553e13
Swarnavo2003 <swarnavo2020@gmail.com>
2026-01-14 16:45:21 +0530

    prompt templating added to chatbot

--- chatbot.py
+++ chatbot.py
@@ -1,18 +1,21 @@
 from langchain_openai import ChatOpenAI
+from langchain_core.messages import SystemMessage, HumanMessage, AIMessage
 from dotenv import load_dotenv
 
 load_dotenv()
 
 model = ChatOpenAI(model="gpt-4o-mini")
-chat_history = []
+chat_history = [
+  SystemMessage(content="You are a helpful assistant.")
+]
 
 while True:
   user_input = input("🤓Your: ")
-  chat_history.append({"role":"user", "content":user_input})
+  chat_history.append(HumanMessage(content=user_input))
   if user_input == "exit":
     break
   result = model.invoke(chat_history)
-  chat_history.append({"role":"assistant", "content":result.content})
+  chat_history.append(AIMessage(content=result.content))
   print(f"🤖AI: {result.content}")
 
 print(chat_history)
